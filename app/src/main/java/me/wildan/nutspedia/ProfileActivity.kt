2fa323355ef2a122429c9a82ed9eb144466df20d
wildanGhifari <wildghifari@gmail.com>
2021-07-02 16:03:47 +0700

    Add intent to Email, Github and Dribbble button

--- app/src/main/java/me/wildan/nutspedia/ProfileActivity.kt
+++ app/src/main/java/me/wildan/nutspedia/ProfileActivity.kt
@@ -1,6 +1,9 @@
 package me.wildan.nutspedia
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
@@ -10,6 +13,25 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         setActionBarTitle(title)
+
+
+        val btnEmail: ImageButton = findViewById(R.id.email)
+        btnEmail.setOnClickListener {
+            val myEmail = Intent(Intent.ACTION_VIEW, Uri.parse("mailto:wildghifari@gmail.com"))
+            startActivity(myEmail)
+        }
+
+        val btnGithub: ImageButton = findViewById(R.id.github)
+        btnGithub.setOnClickListener {
+            val myGitHub = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/wildanGhifari"))
+            startActivity(myGitHub)
+        }
+
+        val btnDribbble: ImageButton = findViewById(R.id.dribbble)
+        btnDribbble.setOnClickListener {
+            val myDribbble = Intent(Intent.ACTION_VIEW, Uri.parse("https://dribbble.com/wildant"))
+            startActivity(myDribbble)
+        }
     }
 
     private fun setActionBarTitle(title: String) {
