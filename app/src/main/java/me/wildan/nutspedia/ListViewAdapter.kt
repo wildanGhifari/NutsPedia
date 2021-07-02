1db6737f118ff44d1bb82a3456faa3c90924daa8
wildanGhifari <wildghifari@gmail.com>
2021-07-02 16:37:15 +0700

    Add toast when btn share is clicked and add id to chat button in profile activity

--- app/src/main/java/me/wildan/nutspedia/ListViewAdapter.kt
+++ app/src/main/java/me/wildan/nutspedia/ListViewAdapter.kt
@@ -1,13 +1,17 @@
 package me.wildan.nutspedia
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+
 
 class ListViewAdapter(private val listNuts: ArrayList<Nuts>) : RecyclerView.Adapter<ListViewAdapter.ListViewHolder>(){
     private lateinit var onItemClickCallback : OnItemClickCallback
@@ -24,6 +28,7 @@ class ListViewAdapter(private val listNuts: ArrayList<Nuts>) : RecyclerView.Adap
         var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
         var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         var tvDesc: TextView = itemView.findViewById(R.id.tv_item_detail)
+        var btnShare: Button = itemView.findViewById(R.id.btn_share)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
@@ -42,6 +47,10 @@ class ListViewAdapter(private val listNuts: ArrayList<Nuts>) : RecyclerView.Adap
         holder.tvName.text = nuts.name
         holder.tvDesc.text = nuts.desc
 
+        holder.btnShare.setOnClickListener {
+            Toast.makeText(holder.itemView.context, "Share " + listNuts[position].name,
+            Toast.LENGTH_SHORT).show()
+        }
         holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listNuts[position]) }
 
     }
